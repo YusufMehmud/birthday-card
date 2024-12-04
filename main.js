4b5e16b160155692e47156d0e8ef6c2f3728efa9
YusufMehmud <65419862+YusufMehmud@users.noreply.github.com>
2024-12-04 23:29:54 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// Optionally, you can add functionality for a click event if you'd like to flip it when clicked
+document.querySelector('.card').addEventListener('click', function () {
+    this.style.transform = 'rotateY(180deg)';
+  });
+  
